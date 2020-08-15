328404e9adf2a3c000a04e5a901535968e5feec5
alphahm <ahmwebdev@gmail.com>
2020-08-15 01:06:08 +0100

    use lambda with map and filter

--- lambda.rkt
+++ lambda.rkt
@@ -2,3 +2,11 @@
 
 ; nameless function aka lambda, define arguments then body
 ((lambda (num) (- num 2)) 5) 
+
+(map
+ (lambda (num) (+ num 1))
+ '(1 2 3 4 5)) ; '(2 3 4 5 6)
+
+(filter
+ (lambda (num) (= (modulo num 10) 1))
+ '(10 12 30 43 50)) ; '(10 30 50)
